b91e9220c0d32a0b2548aebe50e924d6c60baa4b
Irene <ireensc@gmail.com>
2021-09-30 17:54:57 +0200

    crear componente bottomNavigation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <bottomNavigation></bottomNavigation>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import bottomNavigation from '@/components/displayContenido_bottomNavigation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    bottomNavigation
   }
 }
 </script>
---
+++ src/components/displayContenido_bottomNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bottomNavigation bg-secondary h-100">Prueba
+
+
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'bottomNavigation',
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.bottomNavigation{
+  height: 50px;
+}
+
+</style>
