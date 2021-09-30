ee2def6dfdbc203b82df446cb342a1a5a20b6d59
Antoni Blanche <antoni.bx@gmail.com>
2021-09-30 18:15:44 +0200

    scaffolding del LeftNav

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <LeftNavigation :topics="topics" @show="show" />
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -31,8 +32,12 @@
 </template>
 
 <script>
+import LeftNavigation from '../components/LeftNavigation.vue'
 export default {
   name: 'HelloWorld',
+  component: {
+    LeftNavigation
+  },
   props: {
     msg: String
   }
---
+++ src/components/LeftNavigation.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Prova</h1>
+        <div v-for="(topic, index) in topics" :key="index">
+            <div :class="status()" @click="switchStatus">{{index}}. {{topic.title}}</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            actual: 0,
+            visit: []
+        }
+    },
+    props:['topics'],
+    methods:{
+        status(index) {
+            let status;
+            if(this.actual==index) {status='bg-success'}
+            else if (this.visit[index]=='visited') {status = 'bg-primary'}
+            else {status="bg-secondary"}
+            return status;
+        },
+        switchStatus(index) {
+            this.actual = index;
+            this.visit[index]='visited';
+            this.$emit('show', this.actual);
+        }
+    },
+    /*computed:{
+        computed1(){
+            return
+        }
+    },*/
+    mounted() {
+        this.switchStatus(0);
+    }
+}
+</script>
