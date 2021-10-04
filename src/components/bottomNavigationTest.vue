83b729c52e417f8332586d9b2aa9fc398ef532ee
ferranfusalba <ferran@protonmail.ch>
2021-10-04 16:32:50 +0200

    modulePage

---
+++ src/components/bottomNavigationTest.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-6 bg-danger">⬅️</div>
+      <div class="col-6 bg-primary">➡️</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "bottomNavigationTest",
+};
+</script>
