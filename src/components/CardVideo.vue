a813cb5bb2b60bf4f25fbaa58bbbc6a88d369a0a
Sergi Mateo <sergimateo@gmail.com>
2021-10-04 16:44:11 +0200

    frst commit

---
+++ src/components/CardVideo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //   totaltopics: 0,
+      //   topics: ["uno", "dos", "tres"],
+      //   actual: 44,
+      //   visit: [],
+    };
+  },
+  // props: ["topics", "actual"],
+
+  methods: {
+    // actualTopic(index) {
+    //   let status;
+    //   if (this.actual == index) {
+    //     status = "bg-success";
+    //   } else if (this.visit[index] == "visited") {
+    //     status = "bg-primary";
+    //   } else {
+    //     status = "bg-secondary";
+    //   }
+    //   return status;
+    // },
+    // totalTopics() {
+    //   this.totaltopics = this.topics.length;
+    // },
+    // returnToMainMenu() {
+    //   alert("Returning to main menu!");
+    // },
+  },
+  //   },
+  beforeMount() {
+    // this.totalTopics();
+  },
+  //   update() {},
+};
+</script>
