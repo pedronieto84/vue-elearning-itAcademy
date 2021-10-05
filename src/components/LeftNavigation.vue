563d7bbb4c4dcbe6e8dfabcdc154227d423eeec3
Sergi Mateo <sergimateo@gmail.com>
2021-10-05 18:49:41 +0200

    added div in left nav comp

--- src/components/LeftNavigation.vue
+++ src/components/LeftNavigation.vue
@@ -1,44 +1,52 @@
 <template>
+  <div>
     <div v-for="(topic, index) in topics" :key="index" class="text-light">
-        <div :class="status(index)" @click="switchStatus(index)">{{topic.topicId}}. {{topic.title}}</div>
+      <div :class="status(index)" @click="switchStatus(index)">
+        {{ topic.topicId }}. {{ topic.title }}
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            visit: [],
-        }
+  data() {
+    return {
+      visit: [],
+    };
+  },
+  computed: {
+    topics: function() {
+      return this.$store.getters.getTopics;
     },
-    computed: {
-        topics: function() {
-            return this.$store.getters.getTopics;
-        },
-        actual: function() {
-            return this.$store.getters.getActual;
-        }
+    actual: function() {
+      return this.$store.getters.getActual;
     },
-    watch: {
-        actual: function (val) {
-            this.visit[val]='visited';
-        }
+  },
+  watch: {
+    actual: function(val) {
+      this.visit[val] = "visited";
     },
-    methods:{
-        status(index) {
-            let status;
-            if(this.actual==index) {status='bg-success border border-light rounded m-1'}
-            else if (this.visit[index]=='visited') {status = 'bg-primary border border-light rounded m-1'}
-            else {status="bg-secondary border border-light rounded m-1"}
-            return status;
-        },
-        switchStatus(index) {
-            this.visit[index]='visited';
-            this.$store.dispatch('updateActual', index);
-        }
+  },
+  methods: {
+    status(index) {
+      let status;
+      if (this.actual == index) {
+        status = "bg-success border border-light rounded m-1";
+      } else if (this.visit[index] == "visited") {
+        status = "bg-primary border border-light rounded m-1";
+      } else {
+        status = "bg-secondary border border-light rounded m-1";
+      }
+      return status;
     },
-    mounted() {
-        this.switchStatus(0);
-    }
-}
+    switchStatus(index) {
+      this.visit[index] = "visited";
+      this.$store.dispatch("updateActual", index);
+    },
+  },
+  mounted() {
+    this.switchStatus(0);
+  },
+};
 </script>
