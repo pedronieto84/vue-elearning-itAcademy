d4941d70b74a6bc568e9a3e07e1bc3c1ad85219b
Antoni Blanche <antoni.bx@gmail.com>
2021-10-05 16:52:34 +0200

    Conexion de BottomNav y aspecto de modulePage

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,17 +1,5 @@
 <template>
   <div>
-    <div class="progress mt-2">
-      <div
-        class="progress-bar"
-        role="progressbar"
-        style="width: 25%"
-        aria-valuenow="25"
-        aria-valuemin="0"
-        aria-valuemax="100"
-      >
-        25%
-      </div>
-    </div>
     <h1 class="mt-2">Content {{actual}}</h1>
   </div>
 </template>
@@ -28,12 +16,6 @@ export default {
     actual: function() {
       return this.$store.getters.getActual;
     },
-  },
-  beforeMount() {
-    this.actual = this.$store.getters.getActual;
-  },
-  update() {
-    this.actual = this.$store.getters.getActual;
   }
 };
 </script>
