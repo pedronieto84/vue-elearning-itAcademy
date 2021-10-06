d9227d03946e119788080a3eff51988421f38fc7
Sergi Mateo <sergimateo@gmail.com>
2021-10-06 17:23:24 +0200

    videoEnd event & pause time captured

--- src/components/CardVideo.vue
+++ src/components/CardVideo.vue
@@ -4,9 +4,30 @@
       class="mx-auto"
       src="https://www.youtube.com/watch?v=ubL0PpKmGYQ"
       @ready="onReady"
+      @state-change="stateChange"
       ref="youtube"
       :vars="playervars"
     />
+    <div class="buttons mt-2">
+      <button @click="handleClick('playVideo')">
+        Play
+      </button>
+      <button @click="handleClick('pauseVideo')">
+        Pause
+      </button>
+      <button @click="handleClick('stopVideo')">
+        Stop
+      </button>
+      <!-- <button @click="handleClick('resetView')">
+        Reset
+      </button> -->
+      <button @click="handleClick('mute')">
+        Mute
+      </button>
+      <button @click="handleClick('unMute')">
+        Unmute
+      </button>
+    </div>
   </div>
 </template>
 
@@ -19,13 +40,44 @@ export default defineComponent({
   name: "cardVideo",
   data() {
     return {
-      playervars: { autoplay: 0 },
+      playervars: {
+        autoplay: 0,
+        controls: 1,
+      },
     };
   },
   components: { YouTube },
   methods: {
     onReady() {
       this.$refs.youtube.playVideo();
+      console.log(this.$refs.youtube);
+      //Here goes the on load Seekto() with the time in seconds stored in localstore
+      this.$refs.youtube.seekTo(40);
+    },
+    handleClick(event) {
+      console.log("entro a handleClick");
+      this.$refs.youtube[event]();
+
+      if (event == "pauseVideo") {
+        // console.log("pausa");
+        // console.log(event);
+        let time = this.$refs.youtube.getCurrentTime();
+        alert("Video paused at: " + time);
+      }
+    },
+    stateChange(e) {
+      // @state-change="stateChange"
+      // this.$refs.youtube.playVideo();
+      // console.log("Holi, state change");
+      // console.log(e);
+      if (e.data == 0) {
+        alert("Video ended!");
+      }
+      // else if (e.data == 1) {
+      //   console.log("Holi, pause state change");
+      //   let time = this.$refs.youtube.getCurrentTime();
+      //   alert("Video paused at: " + time);
+      // }
     },
   },
 });
