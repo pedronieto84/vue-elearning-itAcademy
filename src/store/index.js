de2a3c8d23b679c97148f8e7fb5d2a02d1703748
ferranfusalba <ferran@protonmail.ch>
2021-10-07 18:41:36 +0200

    final final definitivo?

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,9 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
     state: {
-        /*
-                topics: [
-                    { topicId: 1, title: 'One' },
-                    { topicId: 2, title: 'Two' },
-                    { topicId: 3, title: 'Three' },
-                    { topicId: 4, title: 'Four' },
-                    { topicId: 5, title: 'Five' },
-                    { topicId: 6, title: 'Six' },
-                    { topicId: 7, title: 'Seven' }
-                ],
-                actual: 0, */
-
+        baseURL: 'https://us-central1-asamblea-27a8d.cloudfunctions.net/',
         topics: [{
                 topicId: 1,
                 title: 'Titulo 1',
@@ -21,7 +11,10 @@ export default createStore({
                 position: 1,
                 moduleId: 1,
                 cardId: 1,
-                cardType: 'video'
+                cardType: 'video',
+                card: {
+                    videoUrl: 'https://www.youtube.com/watch?v=iXoViMla9fc'
+                }
             },
             {
                 topicId: 2,
@@ -30,7 +23,29 @@ export default createStore({
                 position: 2,
                 moduleId: 1,
                 cardId: 1,
-                cardType: 'test'
+                cardType: 'test',
+                card: {
+                    testId: 1,
+                    title: 'Title de test',
+                    subtitle: 'Ens referim a tota l`area metropolitana',
+                    question: 'Pregunta quants habitants te Barcelona?',
+                    items: [{
+                            answer: '100 milió',
+                            right: false
+                        },
+                        {
+                            answer: '10 milions',
+                            right: false
+                        }, {
+                            answer: '3 milions',
+                            right: true
+                        }, {
+                            answer: '100 mil',
+                            right: false
+                        },
+
+                    ]
+                }
             },
             {
                 topicId: 1,
@@ -39,7 +54,10 @@ export default createStore({
                 position: 3,
                 moduleId: 1,
                 cardId: 1,
-                cardType: 'list'
+                cardType: 'list',
+                card: {
+                    items: ["Fdklsjfadlkfjdjfajdjfl jdhfkashkjfdah jfkdfjhaljknfjkdkfld jkdjfhsjkdhfda iqowqu ioqww` jdkhkf jdhaf qioqw m iwodqoie jdskha laos sadoa iouireordsaidjld dafjdkahfjahfdafkdah", "kaskjdajsh jdhfkashkjfdah jfkdfjhaljknfjkdkfldjkdjfhs jkdhfda iqowqu ioqww` jdkhkf jdhaf qioqw m iwodqoie jdskha laos sadoaiouireor dsaidjld dafjdkahfjahfdafkdah", "sjkhasda", "sjhdka", "Sajhkdsdkajdskhadhasj hoa que tal ccvfs fsgsg gdsgsf sgsf ro."]
+                }
             },
             {
                 topicId: 1,
@@ -51,8 +69,7 @@ export default createStore({
             },
 
         ],
-        actual: 1,
-        innerWidth: window.innerWidth,
+        actual: 1
     },
     getters: {
         getActual(state) {
@@ -60,6 +77,7 @@ export default createStore({
         },
         getTopics(state) {
             return state.topics;
+
         },
         isDesktop() {
             if (window.innerWidth >= 992) {
@@ -72,11 +90,19 @@ export default createStore({
     mutations: {
         updateActual(state, nouActual) {
             state.actual = nouActual;
+        },
+        updateTopics(state, payload) {
+            console.log(payload);
+            state.topics = payload;
         }
     },
     actions: {
         updateActual({ commit }, actual) {
             commit('updateActual', actual);
-        }
+        },
+        async getTopics({ state, commit }) {
+            const topics = await axios.get(state.baseURL + 'getTopics');
+            commit('updateTopics', topics);
+        },
     }
 });
