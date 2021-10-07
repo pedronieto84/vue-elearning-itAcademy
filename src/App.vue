05a2aa8ff6e4c5392aea8aaa08c4cb6c73acfe95
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-10-07 16:24:41 +0200

    Pongo la ruta del home apuntando a ModulePage

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,19 @@
 <template>
 
   <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-      Vue E-Learning With Bootstrap!
-      <button type="button" class="close" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-
-    <cardList></cardList>
+  
     <router-view></router-view>
   </div>
-  <CardTest />
 
 </template>
 
 <script>
-//import "bootstrap";
-//import "bootstrap/dist/css/bootstrap.min.css";
-import cardList from './components/cardList.vue'
-
-import CardTest from './components/cardTest.vue'
-//import bottomNavigation from '@/components/displayContenido_bottomNavigation.vue'
 
 
 
 export default {
   name: "App",
-
-  components: {
-    cardList,
-    CardTest,
-    //bottomNavigation
-
-  },
+  
   data(){
     return{
       pointsArray: [1,2,3,4,5],
