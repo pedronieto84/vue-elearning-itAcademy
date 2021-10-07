6c73d0b82f61fd5e43acf6cf3c146754a4a2e831
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-10-07 16:25:56 +0200

    Dejoe l maion limpio para empezar a pegar todos los componentes

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
     <h1 class="mt-2">Content {{actual}}</h1>
-    <Snippet />
-    <cardVideo />
+   
   </div>
 </template>
 
 <script>
-import Snippet from '../components/CodeSnippet.vue'
-import cardVideo from "./CardVideo.vue";
+
 export default {
   name: "mainContent",
   components: {
-    Snippet,
-    cardVideo
+  
   },
   data() {
     return {
