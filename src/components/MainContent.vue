b2982a496941a47daeb0d3177531197691683ae6
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-10-07 16:54:44 +0200

    Main Content version beta funcionando.

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -2,16 +2,47 @@
   <div>
     <h1 class="mt-2">
     
-    Content {{ topics[actual]}}</h1>
+    Content {{ topics[actual].title}}
+    </h1>
+    
+    <div v-if="topics[actual].cardType === 'test'">
+   <cardTest />
+
+
+    </div>
+
+    <div v-if="topics[actual].cardType === 'list'"
+>
+<cardList />
+
+    </div>
+
+    <div v-if="topics[actual].cardType === 'text'"
+>
+text
+    </div>
+
+    <div v-if="topics[actual].cardType === 'video'">
+<CardVideo />
+    </div>
+
+    
    
   </div>
 </template>
 
 <script>
 
+import cardList from '../components/cardList.vue'
+import CardVideo from '../components/CardVideo.vue'
+import cardTest from '../components/cardTest.vue'
+
+
 export default {
   name: "mainContent",
   components: {
+    cardList, CardVideo, cardTest
+
   
   },
   data() {
