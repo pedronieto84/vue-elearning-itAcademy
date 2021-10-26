84d5a6316529be3eafc55e0c44b4442c01effefc
Formacio <formacio@formacio.com>
2021-10-26 16:19:34 +0200

    responsive corregit en coursesView i tagsView

--- src/views/tag.vue
+++ src/views/tag.vue
@@ -28,29 +28,32 @@
         </div>
       </div>
     </div>
-    <!--
-    <div v-else-if="innerWidth() <= 992">
+    <div v-else-if="innerWidth() <= 991">
       <div class="d-flex justify-content-center">
         <div class="col-12 pl-0 pr-0">
           <div v-for="(course, index) in courses" :key="index" class="col-12">
-            <CourseCard
-              class="mt-4 mb-4 ml-2 mr-2"
-              :course="course"
-              :snipNumber="4"
-            />
+            <div v-for="tag in course.tags" :key="tag">
+              <div v-if="tagId == tag.tagId">
+                <CourseCard class="mt-4 mb-4 ml-2 mr-2" :course="course" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div v-else-if="innerWidth() >= 993 && innerWidth() <= 1199">
+    <div v-else-if="innerWidth() >= 992 && innerWidth() <= 1199">
       <div class="d-flex justify-content-center">
         <div class="col-12">
-          <div v-for="(course, index) in courses" :key="index" class="col-6">
-            <CourseCard class="m-2" :course="course" :snipNumber="7" />
+          <div v-for="(course, index) in courses" :key="index" class="col-12">
+            <div v-for="tag in course.tags" :key="tag">
+              <div v-if="tagId == tag.tagId">
+                <CourseCard class="m-2" :course="course" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
