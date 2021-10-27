d01765d14348b9d14702b99f42dfe49ea97f1429
Formacio <formacio@formacio.com>
2021-10-27 15:03:22 +0200

    updates

--- src/views/courses.vue
+++ src/views/courses.vue
@@ -13,7 +13,11 @@
     <div v-else-if="innerWidth() >= 992 && innerWidth() <= 1199">
       <div class="d-flex justify-content-center">
         <div class="col-12 pl-0 pr-0">
-          <div v-for="(course, index) in coursesFiltered" :key="index" class="col-12">
+          <div
+            v-for="(course, index) in coursesFiltered"
+            :key="index"
+            class="col-12"
+          >
             <CourseCard
               class="mt-4 mb-4 ml-2 mr-2"
               :course="course"
@@ -26,7 +30,11 @@
     <div v-else-if="innerWidth() <= 991">
       <div class="d-flex justify-content-center">
         <div class="col-12">
-          <div v-for="(course, index) in coursesFiltered" :key="index" class="col-6">
+          <div
+            v-for="(course, index) in coursesFiltered"
+            :key="index"
+            class="col-6"
+          >
             <CourseCard class="m-2" :course="course" :snipNumber="7" />
           </div>
         </div>
@@ -36,58 +44,50 @@
 </template>
 
 <script>
-import CourseCard from '../components/courseList/courseCard.vue'
-  export default {
-    name: "Courses",
-    watch: {
-      $route(to, from){
-        console.log('watcher', to, from);
-        this.tagId = to.params.tagId
-
+import CourseCard from "../components/courseList/courseCard.vue";
+export default {
+  name: "Courses",
+  watch: {
+    $route(to, from) {
+      console.log("watcher", to, from);
+      this.tagId = to.params.tagId;
+    },
+  },
+  components: {
+    CourseCard,
+  },
+  computed: {
+    coursesFiltered() {
+      if (this.tagId === null) {
+        return this.courses;
+      } else {
+        return this.courses.filter((course) => {
+          return course.tags.some((tag) => {
+            return tag.tagId == this.tagId;
+          });
+        });
       }
     },
-    components: {
-      CourseCard
+  },
+  data() {
+    return {
+      courses: [],
+      tagId: null,
+    };
+  },
+  methods: {
+    innerWidth: function () {
+      return this.$store.getters.innerWidth;
     },
-    computed: {
-      coursesFiltered(){
-        if(this.tagId === null){
-          return this.courses
-        }else{
-          return this.courses.filter((course)=>{
-            return course.tags.some((tag)=>{
-              return tag.tagId == this.tagId
-            })
-          })
-        }
-      }
-    },
-    data(){
-      return{
-       
-        courses: [],
-        tagId: null
-      }
-    },
-  
-    methods: {
-      innerWidth: function () {
-        return this.$store.getters.innerWidth;
-      },
-    },
-  
-    mounted() {
-      this.courses = this.$store.state.courses;
-      console.log('mounted');
-    },
+  },
+  mounted() {
+    this.courses = this.$store.state.courses;
+    this.coursesBBDD = this.$store.state.courses;
+    console.log("mounted");
   },
   created() {
     this.courses = this.$store.state.courses;
     //this.$store.dispatch('getCourses');
-  },
-  mounted() {
-    this.coursesBBDD = this.$store.state.courses;
-    console.log("mounted");
   },
 };
 </script>
