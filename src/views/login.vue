2bef233805c761e87402d2680b3fb6e6f5602bfb
Joserobleses <jrobleses@gmail.com>
2021-11-17 19:01:02 +0100

    activacion de button de form login mediante validacion

--- src/views/login.vue
+++ src/views/login.vue
@@ -14,18 +14,18 @@
             >
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="email" class="form-control" id="loginEmail" name="loginEmail" v-model="email" placeholder="Enter email">
+                    <input type="text" class="form-control" id="loginEmail" name="loginEmail" v-model="email" placeholder="Enter an email">
                 </div>
                 <div class="form-group">
                     <label for="password">Password</label>
-                    <input type="password" class="form-control" id="loginPassword" name="loginPassword" v-model="password" placeholder="Enter password">
+                    <input type="text" class="form-control" id="loginPassword" name="loginPassword" v-model="password"  placeholder="Enter an password">
                 </div>
-                <button type="submit" class="btn btn-primary" disabled>Entrar</button>
+                <button type="submit" class="btn btn-primary" :disabled="actived">Entrar</button>
             </form>
             <p v-if="errors.length">
                 <b>Error</b>
                 <ul class="list-group">
-                        <li  class="list-group-item" v-for="error in errors"  v-bind:key="error" >{{ error }}</li>
+                        <li  class="list-group-item text-danger" v-for="error in errors"  v-bind:key="error" ><p class="alert alert-danger" role="alert" >{{ error }}</p></li>
                 </ul>
             </p>
         </div>
@@ -40,31 +40,47 @@ export default {
         errors:[],
         email:null,
         password:null,
-        enabledisable: true
+        //actived:true
         };
     },
     methods:{
         loginForm: function (e){
             
             if(!this.email){
-                this.errors.push("write a email address, please");
+                this.errors.push("write an email address, please");
             }else if (!this.validEmail(this.email)){
                 this.errors.push("invalid email address");
             }
             if(!this.password){
-                this.errors.push("write password, please");
+                this.errors.push("write a correct password, please");
             }
-            if (this.validEmail(this.email) && this.password){}
+            if(this.password.length < 5 || this.password.length > 9){
+                this.errors.push("write password between 5 and 10 characters, please");
+            }
+           
             if (!this.errors.length){
                 return true;
             }
+            //if (this.validEmail(this.email)) 
             e.preventDefault();
         },
         validEmail: function (email) {
             //return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email);
             return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,4})+$/.test(email);
+        },
+        validPassword: function (password) {
+            //return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email);
+            return /^[a-z0-9]+$/i.test(password);
+        }
+        
+    },
+    computed:{
+      checkVal: function(){
+          
+        return this.validEmail(this.email) && (this.password.length >=5 && this.password.length <=10) && this.validPassword(this.password) ? false : true;
     }
     }
+    
 
 }
 /*
