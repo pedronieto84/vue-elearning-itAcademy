e1c8d6d76c4ad5b15ccd346e67a83004a79859c3
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-07 18:39:06 +0100

    getProjects

--- src/components/competitionPage/CheckResult.vue
+++ src/components/competitionPage/CheckResult.vue
@@ -21,7 +21,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>afsdf
     </div>
 </template>
 
--- src/components/competitionPage/UserCard.vue
+++ src/components/competitionPage/UserCard.vue
@@ -13,6 +13,6 @@ export default {
     data() {
         return {
         };
-    }
+    }asdfasdf
 }
 </script>
